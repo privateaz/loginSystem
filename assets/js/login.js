8ca58d77474adbb1a6d640a49abe3a31b46d7e0c
Matheus Az <private_az@hotmail.com>
2023-08-01 11:44:40 -0300

    Added and changing messages in login system

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,14 @@
-const [loginUsername, loginPassword] = document.querySelectorAll("#loginUsername, #loginPassword");
+const [loginUsername, loginPassword] = document.querySelectorAll(
+  "#loginUsername, #loginPassword"
+);
 
-const btnLogin = document.getElementById("btnLogin"); 
+const btnLogin = document.getElementById("btnLogin");
 
 btnLogin.addEventListener("click", (event) => {
   event.preventDefault();
+  clearMessages("success-text");
+  clearMessages("error-text");
+
   const loginUsernameValue = loginUsername.value;
   const loginPasswordValue = loginPassword.value;
 
@@ -12,16 +17,27 @@ btnLogin.addEventListener("click", (event) => {
 
   const user = users.find((user) => user.username === loginUsernameValue);
 
-  if (!user) {
-    alert("User not found!"); 
+  if (!user || loginPasswordValue !== user.password) {
+    createMessage(loginPassword, `Invalid credentials`, "error-text");
     return;
   }
 
-  if (loginPasswordValue !== user.password) {
-    alert("Invalid credentials!");
-  } else {
-    alert("Success! You are logged in."); 
+  if (loginPasswordValue === user.password) {
+    createMessage(loginPassword, `Login successful!`, "success-text");
     loginUsername.value = "";
-    loginPassword.value = "";    
+    loginPassword.value = "";
   }
 });
+
+function createMessage(beforeCamp, message, type) {
+  if (beforeCamp.classList.contains(type)) return;
+  const div = document.createElement("div");
+  div.textContent = message;
+  div.classList.add(type);
+  beforeCamp.parentNode.insertBefore(div, beforeCamp.nextSibling);
+}
+
+function clearMessages(type) {
+  const messages = document.querySelectorAll("." + type);
+  messages.forEach((message) => message.remove());
+}
